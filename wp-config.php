c9728f380317abe9a78f9f350f7b9695e2dd3bf2
tim0thius <timblack.design@gmail.com>
2018-07-13 08:50:42 -0500

    Add images and debug

--- wp-config.php
+++ wp-config.php
@@ -65,6 +65,24 @@ $table_prefix = 'wp_';
 
 
 
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
+
+
+
 
 
 /* Inserted by Local by Flywheel. See: http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy */
