1ab768b23c9d894f4350649d1a1d278d655812b0
tim0thius <timblack.design@gmail.com>
2018-08-08 16:31:43 -0500

    Add contact us template

---
+++ wp-content/themes/blankslate/page-contact-us.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+@include( 'templates/components/subpage-header2' )
+
+<section id="" class="block-page" role="main" >
+	<div class="grid__container">
+		<div class="grid">
+			<div class="grid__item one-whole desk--one-half">
+				<div class="block-page__content">
+					<div class="block-page__content-inner">
+
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="header">
+						<h1 class="block-page__headline"><?php the_title(); ?></h1>
+						</header>
+						<section class="block-page__content">
+						<?php
+							$formid = get_field('form_id');
+
+						?>
+
+						<?php echo do_shortcode( '[ninja_form id=$formid]' ); ?>
+					
+						</section>
+						</article>
+
+					</div>
+				</div>
+			</div><!-- 
+			 --><div class="grid__item one-whole desk--one-half">
+				<div class="block-page__imagebox">
+					<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+				</div>
+			</div>
+		</div>
+		<div class="grid">
+			<div class="grid__item one-whole">
+				<div class="block-page__content">
+					<div class="block-page__content-inner">
+
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<section class="block-page__content">
+						<?php the_content(); ?>
+						</section>
+						</article>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</section>
+
+<?php endwhile; endif; ?>
+</section>
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
