36258adf70633e7c8b6da55cdec0691e93402771
tim0thius <timblack.design@gmail.com>
2018-08-28 14:44:37 -0500

    Cleans up testimonials and teams. Adds team to about page

--- wp-content/themes/blankslate/page-testimonials.php
+++ wp-content/themes/blankslate/page-testimonials.php
@@ -1,38 +1,56 @@
 <?php get_header(); ?>
 <?php
-	$testimonial_args = array('post_type' => 'testimonial');
+	$testimonial_args = array(
+		'post_type' => 'testimonial',
+		'meta_key'			=> 'category',
+		'orderby'			=> 'meta_value',
+		'order'				=> 'DESC'
+	);
 	$testimonial_posts = new WP_Query($testimonial_args);
 ?>
 
 @include( 'templates/components/subpage-header2' )
 
+<?php if (have_posts()) :while ( have_posts() ) : the_post(); ?>
+   
+
 <section id="" class="block-page block-page--testimonials" role="main" >
 	<div class="grid__container">
-		<h1 class="block-page__headline">Testimonials</h1>
+		<h1 class="block-page__headline"><?php the_title(); ?></h1>
+
+		<?php if(get_the_content()): ?>
+			<div class="block-page__content">
+				<?php the_content(); ?>
+			</div>
+		<?php endif; ?>
 		<div class="grid">
 			<?php
-
-							   if($testimonial_posts->have_posts()) : 
-							   	$counter = 0;
-							   	$testimonials_count = $testimonial_posts->found_posts;
-							      while($testimonial_posts->have_posts()) : 
-							         $testimonial_posts->the_post();			    
-							?><!-- 
-							 -->
-							 <div class="grid__item">
-							 @include( 'templates/cards/testimonial-item')
-							 </div><!-- 
-							 --><?php
-							 		$counter ++;
-							         if($counter == 9){break;}
-							      endwhile;
-							   else: 
-							?>
-							    Oops, there are no testimonials.
-							<?php
-							   endif;
-							?>	
+			   if($testimonial_posts->have_posts()) : 
+			   	$counter = 0;
+			   	$testimonials_count = $testimonial_posts->found_posts;
+			      while($testimonial_posts->have_posts()) : 
+			         $testimonial_posts->the_post();			    
+			?><!-- 
+			 -->
+			 <div class="grid__item">
+			 @include( 'templates/cards/testimonial-item')
+			 </div><!-- 
+			 --><?php
+			 		$counter ++;
+			      endwhile;
+			   else: 
+			?>
+			    Oops, there are no testimonials.
+			<?php
+			   endif;
+			   wp_reset_postdata();
+			?>	
+			
 		</div>
 	</div>
 </section>
+
+<?php endwhile; else : ?>
+<p>Sorry no posts matched your criteria.</p>
+<?php endif; ?>
 <?php get_footer(); ?>
