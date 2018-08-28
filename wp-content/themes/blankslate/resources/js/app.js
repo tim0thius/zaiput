b5647e5d85ce8545d4a3164344329c182346e611
tim0thius <timblack.design@gmail.com>
2018-08-28 16:16:13 -0500

    faq js

--- wp-content/themes/blankslate/resources/js/app.js
+++ wp-content/themes/blankslate/resources/js/app.js
@@ -7,60 +7,6 @@ jQuery(document).ready(function ($) {
 		e.preventDefault();
 	});
 
-
-
-
-	// $('.action--show-slideshow').click(function(e){
-	// 	var initialSlideIndex = $(this).attr('data-index') - 1 + 1 // no idea why this works
-
-	// 		$(".block-gallery__slider").slick({
-	// 		  // normal options...
-	// 		  initialSlide: initialSlideIndex,
-	// 		  infinite: true,
-	// 		  dots: true, 
-	// 		  adaptiveHeight: true
-	// 		});
-
-	// 	$('body').addClass('state--slideshow-visible')
-	// })
-
-	// $('.action--hide-slideshow').click(function(){
-	// 	$('body').removeClass('state--slideshow-visible')
-	// 	$('.block-gallery__slider').slick('unslick');
-	// })
-
-	// $(".block-hero__slider").slick({
- 
-	//   // normal options...
-	//   infinite: true,
-	//   dots: false,
-	//   lazyload: 'ondemand',
-	//   // the magic
-	//   responsive: [{
-	 
-	//       breakpoint: 1024,
-	//       settings: {
-	//         slidesToShow: 1,
-	//         infinite: true,
-	//         dots: true
-	//       }
-	 
-	//     }, {
-	 
-	//       breakpoint: 600,
-	//       settings: {
-	//         slidesToShow: 1,
-	//         dots: true
-	//       }
-	 
-	//     }, {
-	 
-	//       breakpoint: 300,
-	//       settings: "unslick" // destroys slick
-	 
-	//     }]
-	// });	
-
 	$(".block-highlights__testimonials-slider").slick({
 	  // normal options...
 	  infinite: true,
@@ -76,5 +22,9 @@ jQuery(document).ready(function ($) {
 
 		$('.block-product-tabs__content[id="'+ targetId +'"]').addClass('active').siblings().removeClass('active');
 		console.log(targetId);
+	});
+
+	$('.component-faq__question').click(function(){
+		$(this).parent().toggleClass('state--open');
 	});
 })
