3a6791e59228a983362e9a813e7a322d56491f88
tim0thius <timblack.design@gmail.com>
2018-08-29 11:09:15 -0500

    Update pub links to only link if url is exists

--- wp-content/themes/blankslate/templates/blocks/highlights.php
+++ wp-content/themes/blankslate/templates/blocks/highlights.php
@@ -110,7 +110,13 @@
 				 					<div class="block-highlights__publications-logos">
 										<?php if( have_rows('publications_logos', 'option') ): ?>
 										    <?php while( have_rows('publications_logos', 'option') ): the_row(); ?>
-										        <a class="block-highlights__publication_link" href="<?php the_sub_field('publication_url'); ?>"><img src="<?php the_sub_field('publication_logo'); ?>" class="block-highlights__publication_logo" /></a>
+										    	<?php if(get_sub_field('publication_url')): ?>
+										    		<a class="block-highlights__publication_link" href="<?php the_sub_field('publication_url'); ?>">
+										    	<?php endif; ?>
+										    		<img src="<?php the_sub_field('publication_logo'); ?>" class="block-highlights__publication_logo" />
+										    	<?php if(get_sub_field('publication_url')): ?>
+										    		</a>
+										    	<?php endif; ?>
 										    <?php endwhile; ?>
 										<?php endif; ?>
 				 					</div>			
