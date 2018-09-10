74c7878ab3a23f4625ca547ed0367e0b816d89d3
tim0thius <timblack.design@gmail.com>
2018-09-10 12:09:33 -0500

    Create templates

---
+++ wp-content/themes/blankslate/template-announcements.php
@@ -0,0 +1,13 @@
+<?php
+
+    /*
+        Template Name: Announcements
+     */
+
+    get_header();
+?>
+@include( 'templates/components/subpage-header2' )
+
+@include( 'templates/blocks/announcement-tabs' )
+
+<?php get_footer(); ?>
