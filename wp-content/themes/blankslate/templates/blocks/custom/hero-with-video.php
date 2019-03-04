5325748ca02ea4817c894c0e89ff2ad0f9b6770f
tim0thius <timblack.design@gmail.com>
2019-03-04 10:21:48 -0600

    Adds left-right functionality to video section. Adds height option to here. Tweaks video spacing

--- wp-content/themes/blankslate/templates/blocks/custom/hero-with-video.php
+++ wp-content/themes/blankslate/templates/blocks/custom/hero-with-video.php
@@ -1,4 +1,4 @@
-<section id="block-hero--" class="block-hero block-hero--custom video--<?php the_sub_field('video_alignment') ?>">
+<section id="block-hero--" class="block-hero block-hero--custom video--<?php the_sub_field('video_alignment') ?> video--<?php the_sub_field('video_type') ?>">
 	<div class="grid__super-container-wrap" style="background-image: url(<?php the_sub_field('bg_img') ?>);">
 	<div class="grid__super-container">
 
@@ -10,28 +10,38 @@
 					</div>
 				</div><!-- 
 				 --><div class="grid__item one-half portable--one-whole">
-					<div class="block-hero__video" >
+					<div class="block-hero__video-wrapper" >
+						<div class="block-hero__video" >
 
-					<!-- Need logic to switch based on video_typ subfield -->
-						
-						<?php // the_sub_field('oembed_video'); ?>
-						<?php 
+							<!-- Need logic to switch based on video_typ subfield -->
+							<?php 
 
-							$file = get_sub_field('local_video');
-							$url = $file['url'];
-							$title = $file['title'];
-							$caption = $file['caption'];
-							$type = $file['mime_type'];
+								$file = get_sub_field('local_video');
+								$url = $file['url'];
+								$title = $file['title'];
+								$caption = $file['caption'];
+								$type = $file['mime_type'];
 
-							// echo "<pre style='background-color: white;font-size: 18px; line-height: 1.5; font-weight: bold;' >";
-							//var_dump($file);
-							// echo "</pre>";
+								// echo "<pre style='background-color: white;font-size: 18px; line-height: 1.5; font-weight: bold;' >";
+								//var_dump($file);
+								// echo "</pre>";
+							?>
+								
+							<?php if(get_sub_field('video_type') == 'oembed'): ?>
+								<?php the_sub_field('oembed_video'); ?>
+							<?php endif; ?>
 
-						?>
-						<video autoplay controls muted loop id="block-hero__bg-video" class="block-hero__bg-video" style="display: none;" >
-						  <source src="<?php echo $url ?>" type="<?php echo $type; ?>">
-						</video>
-						<iframe width="100%" height="" src="https://www.youtube.com/embed/<?php the_sub_field('youtube_video_id'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							<?php if(get_sub_field('video_type') == 'local'): ?>
+								<video autoplay controls muted loop id="block-hero__bg-video" class="block-hero__bg-video" >
+									<source src="<?php echo $url ?>" type="<?php echo $type; ?>">
+								</video>
+							<?php endif; ?>
+
+								<?php if(get_sub_field('video_type') == 'youtube'): ?>
+									<iframe width="100%" height="" src="https://www.youtube.com/embed/<?php the_sub_field('youtube_video_id'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+								<?php endif; ?>
+							
+						</div>		
 					</div>		
 				</div>
 			</div>
