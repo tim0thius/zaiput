70d2d12fbb6d240d23845542e38b9c7b54719fbe
tim0thius <timblack.design@gmail.com>
2019-06-07 10:29:43 -0500

    Wrap up custom template

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -8,10 +8,23 @@
 	<!-- <link rel="stylesheet" href="https://use.typekit.net/dey5dnl.css"> -->
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 	<?php wp_head(); ?>
+
+	<?php if (get_field('use_alt_header')) : ?>
+		<style>
+			.block-header__box {
+				background-color: <?php the_field('header_background_color') ?>;
+				color: #fff;
+			}
+		</style>
+	<?php endif; ?>
 </head>
 
+<?php
+$header_class = get_field('use_alt_header') ? 'block-header--blue' : '';
+?>
+
 <body <?php body_class(); ?>>
-	<div id="wrapper" class="hfeed">
+	<div id="wrapper" class="hfeed block-header__box <?php echo $header_class ?>">
 		<div class="grid__container">
 			<header class="block-header" role="banner">
 				<section class="block-header__branding">
@@ -20,7 +33,14 @@
 							echo '<h1>';
 						} ?>
 						<a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_html(get_bloginfo('name')); ?>" rel="home">
-							<img class="block-header__logo" src="<?php the_field('header_logo', 'option') ?>" alt="<?php echo esc_html(get_bloginfo('name')); ?>">
+							<?php if (get_field('use_alt_header')) : ?>
+								<img class="block-header__logo alt-logo" src="<?php the_field('header_logo_alt', 'option') ?>" alt="<?php echo esc_html(get_bloginfo('name')); ?>">
+							<?php else : ?>
+								<img class="block-header__logo" src="<?php the_field('header_logo', 'option') ?>" alt="<?php echo esc_html(get_bloginfo('name')); ?>">
+							<?php endif; ?>
+
+
+
 						</a>
 						<?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
 							echo '</h1>';
@@ -36,7 +56,7 @@
 
 				<nav class="block-header--menu" role="navigation">
 					<!-- <div id="search">
-            		<?php 
+            		<?php
 					?>
             		</div> -->
 					<?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
@@ -48,4 +68,6 @@
 				</a>
 			</header>
 		</div>
+	</div>
+	<div class="wrapper">
 		<div id="container">
