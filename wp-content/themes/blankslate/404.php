595a3d186e158884d2e949e8248fc3bebff43a71
tim0thius <timblack.design@gmail.com>
2020-02-10 07:17:05 -0600

    Updat 404 page

--- wp-content/themes/blankslate/404.php
+++ wp-content/themes/blankslate/404.php
@@ -1,14 +1,30 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Not Found', 'blankslate' ); ?></h1>
-</header>
-<section class="entry-content">
-<p><?php _e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
+
+
+@include( 'templates/components/subpage-header2' )
+
+<section id="" class="block-page block-page--products" role="main" >
+	<div class="grid__container">
+		<div class="grid">
+			<div class="grid__item one-whole">
+				<header class="header">
+					<h1 class="block-page__headline"><?php _e( '404 | Page Not Found', 'blankslate' ); ?></h1>
+				</header>
+				<section class="block-page__content">
+					<?php _e( 'Nothing found for the requested page. Please select a product below or choose a page from the menu.', 'blankslate' ); ?>
+					<div class="block-page__links"><?php wp_link_pages(); ?></div>
+				</section>
+			</div>
+		</div>
+
+		@include( 'templates/blocks/products--frontpage', ['product_id' => 'products'] )
+		
+	</div>
 </section>
-</article>
-</section>
-<?php get_sidebar(); ?>
+
+
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
+
+
+
