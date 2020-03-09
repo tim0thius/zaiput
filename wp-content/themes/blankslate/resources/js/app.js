dca4567ec6496e1315e2a5263702c62ceb7c9ad2
tim0thius <timblack.design@gmail.com>
2020-03-09 09:13:50 -0500

    Sets up active tab via url hash

--- wp-content/themes/blankslate/resources/js/app.js
+++ wp-content/themes/blankslate/resources/js/app.js
@@ -1,6 +1,7 @@
 require('jquery');
 
 jQuery(document).ready(function ($) {
+
 	$('.action--toggle-menu').click(function(e){
 		$('.block-header--menu').toggleClass('state--open');
 		$('.block-header__menu-toggle').toggleClass('state--open');
@@ -28,7 +29,12 @@ jQuery(document).ready(function ($) {
 		$(this).parent().toggleClass('state--open');
 	});
 
+    if($('.block-vertical-tabs__tab').length){
+        set_tab_via_url();
+    }
+
     video_fix_for_chrome();
+    
 })
 
 function video_fix_for_chrome() {
@@ -40,5 +46,35 @@ function video_fix_for_chrome() {
         jQuery('#block-hero--frontpage').addClass('video--youtube');
         jQuery('body').addClass('is-ie');
     }
+}
+
+function set_tab_via_url() {
+
+    page_hash = window.location.hash;
+
+    // window.location.hash = '';
+    tab_target = page_hash.replace('#', '');
+    tab_target  = tab_target.replace(/^_/, '');
+    scroll_to_target = '#' + jQuery('.block-vertical-tabs').first().attr('id');
+    console.log(scroll_to_target);
+    // scroll_to_target = jQuery('#block-product-tabs--multistage-extraction');
+
+
+    if(tab_target != ''){
+        scrollToTabs(tab_target, scroll_to_target);
+    }   
+}
+
+function scrollToTabs(f_tab_target, f_scroll_to_target) {
+    jQuery(`[data-tab-target='${f_tab_target}']`).click();
+    setTimeout(function(){
+        jQuery('html, body').animate({
+        scrollTop: jQuery(scroll_to_target).offset().top
+      }, 600, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }, 500);
 }
 // setTimeout(video_fix_for_chrome, 2000);
